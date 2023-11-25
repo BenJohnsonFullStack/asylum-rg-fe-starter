6f0164e72a369f6e64df3b585aeeab9a2d79364d
Ben Johnson <monk.sound.design@gmail.com>
2023-11-25 18:05:48 -0500

    profile page with basic info

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useHistory } from 'react-router-dom';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  let history = useHistory();
+
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    history.push('/');
+  }
+
+  return (
+    <>
+      <h1>Welcome Back!</h1>
+      <h3>{user.name}</h3>
+      <h3>{user.email}</h3>
+    </>
+  );
 };
 
 export default Profile;
